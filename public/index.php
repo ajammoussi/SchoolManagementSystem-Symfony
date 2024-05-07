e322c95c39c995d501ae031cbcc60c282a3858d6
papy1919 <mohamedselim.oueslati@insat.ucar.tn>
2024-05-07 23:00:42 +0200

    working but still a prob with css

--- public/index.php
+++ public/index.php
@@ -2,7 +2,8 @@
 
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+// Adjust the path to autoload_runtime.php if needed
+require_once __DIR__ . '/../vendor/autoload_runtime.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
--- src/Controller/LandingpageController.php
+++ src/Controller/LandingpageController.php
@@ -4,15 +4,16 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
-class LandingpageController
+class LandingpageController extends AbstractController
 {
     #[Route('/landingpage', name: 'app_landingpage')]
     public function index(): Response
     {
-        return $this->render('landingpage/landingpage.html.twig', [
+        return $this->render('landingpage/index.html.twig', [
             'controller_name' => 'LandingpageController',
         ]);
     }
 }
+
