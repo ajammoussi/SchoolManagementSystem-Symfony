86f365c47e0c9551953d924d6122adb5bf44cbb0
iyed Mdimegh <iyedmdimegh@gmail.com>
2024-05-07 01:41:17 +0100

    configured admin templates, controller, and admin's statistics page

---
+++ src/Repository/AbsenceRepository.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Absence;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Absence>
+ */
+class AbsenceRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Absence::class);
+    }
+
+    public function absencesStatistics(): array
+    {
+        $qb = $this->createQueryBuilder('a');
+        $qb->select('a.absencedate,count(a) AS nbAbsences')
+            ->groupBy('a.absencedate');
+        return $qb->getQuery()->getResult();
+
+    }
+//    /**
+//     * @return Absence[] Returns an array of Absence objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('a')
+//            ->andWhere('a.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('a.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Absence
+//    {
+//        return $this->createQueryBuilder('a')
+//            ->andWhere('a.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
