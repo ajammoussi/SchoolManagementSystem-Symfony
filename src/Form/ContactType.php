c12d135438a9cc0e5854c1d59392c2c3df9023ba
papy1919 <mohamedselim.oueslati@insat.ucar.tn>
2024-05-09 23:04:01 +0200

    last version form with mailpit server

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controllers;
+namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
@@ -10,7 +10,7 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ContactFormType extends AbstractType
+class ContactType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
