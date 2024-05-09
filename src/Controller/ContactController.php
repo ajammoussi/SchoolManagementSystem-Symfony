30eeb5e97b74cef94f5e2a085b3ddc0a276b5c7c
Maher-Wali <walimaher00@gmail.com>
2024-05-09 02:27:38 +0100

    contact form should be working

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactFormType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+
+class ContactController extends AbstractController
+{
+    private $mailer;
+
+    public function __construct(MailerInterface $mailer)
+    {
+        $this->mailer = $mailer;
+    }
+
+    #[Route('/contact', name: 'contact')]
+    public function contact(Request $request, MailerInterface $mailer): Response
+    {
+        $form = $this->createForm(ContactFormType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $formData = $form->getData();
+
+            $email = (new Email())
+                ->from($formData['email'])
+                ->to('walimaher00@gmail.com')
+                ->subject('New Contact Message')
+                ->html(
+                    $this->renderView(
+                        'emails/contact_message.html.twig',
+                        ['data' => $formData]
+                    )
+                );
+
+            $mailer->send($email);
+
+            $this->addFlash('success', 'Your message has been sent successfully!');
+
+            return $this->redirectToRoute('app_landingpage');
+        }
+
+        return $this->render('landingpage/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
---
+++ src/Controller/LandingpageController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Form\ContactFormType;
+
+class LandingpageController extends AbstractController
+{
+    #[Route('/landingpage', name: 'app_landingpage')]
+    public function index(): Response
+    {
+        $form = $this->createForm(ContactFormType::class);
+
+        return $this->render('landingpage/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
+
